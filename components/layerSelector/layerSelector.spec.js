4dc379230a516c12cb681d5181660472f45f9658
racheliff <racheliff@gmail.com>
2016-02-20 23:05:27 +0200

    Update layerSelector.spec.js

--- components/layerSelector/layerSelector.spec.js
+++ components/layerSelector/layerSelector.spec.js
@@ -1,26 +1,43 @@
 describe('Layer Selector tests', function(){
-    var $compile,
-        $rootScope,
-        element;
+    it('Should pass', function(){
+        expect(true).toBeTruthy();
+    })
+});
 
-    // Load the myApp module, which contains the directive
-    beforeEach(module('ui-leaflet-extensions'));
+define(function(require) {
+    'use strict';
+    /*require('angular');
+    require('angularMock');
+    require('components/layerSelector/layerSelector.js');*/
 
-    // Store references to $rootScope and $compile
-    // so they are available to all tests in this describe block
-    beforeEach(inject(function(_$compile_, _$rootScope_){
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        $compile = _$compile_;
-        $rootScope = _$rootScope_;
-        $rootScope.layersSelectorData = {name:"option 1"};
-        // Compile a piece of HTML containing the directive
-        element = $compile('<ui-leaflet-layers-selector items="layersSelectorData" ng-model="item"></ui-leaflet-layers-selector>')($rootScope);
-        // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
-        $rootScope.$digest();
-    }));
+    describe('Unit testing Layer Selector', function () {
+        var $compile,
+            $rootScope;
 
-    it('Replaces the element with the appropriate content', function() {
-        // Check that the compiled element contains the templated content
-        expect(element.html()).toContain("lidless, wreathed in flame, 2 times");
+        // Load the ui-leaflet-extensions module, which contains the directive
+        beforeEach(module('ui-leaflet-extensions'));
+
+        // Store references to $rootScope and $compile
+        // so they are available to all tests in this describe block
+        beforeEach(inject(function (_$compile_, _$rootScope_) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        }));
+
+        it('Replaces the element with the appropriate content', function () {
+
+            this.layersSelectorData = {
+                option1: {name: "option 1"},
+                option2: {name: "option 2"}
+            };
+            this.item = {option1: {name: "option 1"}};
+            // Compile a the component: ui-leaflet-layers-selector
+            var element = $compile('<ui-leaflet-layers-selector items="layersSelectorData" ng-model="item"></ui-leaflet-layers-selector>')($rootScope);
+            // fire all the watches
+            $rootScope.$digest();
+            // Check that the compiled element contains the templated content
+            expect(element.find('li').length).toBe(2);
+        });
     });
 });

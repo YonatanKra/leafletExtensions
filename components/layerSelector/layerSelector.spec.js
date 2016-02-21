93f8b2ada690f0549f151bc182bf308cc8b55bcc
Yonatan <kra.yonatan@gmail.com>
2016-02-21 14:28:37 +0200

    Unit tests ready to work

--- components/layerSelector/layerSelector.spec.js
+++ components/layerSelector/layerSelector.spec.js
@@ -1,43 +1,44 @@
-describe('Layer Selector tests', function(){
-    it('Should pass', function(){
-        expect(true).toBeTruthy();
-    })
-});
+describe('Unit testing Layer Selector', function () {
+    var $compile,
+        $rootScope,
+        element,
+        uiLeafletModule;
 
-define(function(require) {
-    'use strict';
-    /*require('angular');
-    require('angularMock');
-    require('components/layerSelector/layerSelector.js');*/
+    var $httpBackend;
 
-    describe('Unit testing Layer Selector', function () {
-        var $compile,
-            $rootScope;
+    // Load the ui-leaflet-extensions module, which contains the directive
+    beforeEach(function(){
+        uiLeafletModule = angular.module('ui-leaflet', []); // mock the ui-leaflet module
+        module('ui-leaflet-extensions');
+    });
 
-        // Load the ui-leaflet-extensions module, which contains the directive
-        beforeEach(module('ui-leaflet-extensions'));
+    // Store references to $rootScope and $compile
+    // so they are available to all tests in this describe block
+    beforeEach(inject(function (_$compile_, _$rootScope_, _$injector_) {
+        // use httpBackend to accept template calls
+        $httpBackend = _$injector_.get('$httpBackend');
+        $httpBackend.whenGET( /^(.*\.(?=(htm|html|svg|js)$))?[^.]*$/i ).respond(200, '');
 
-        // Store references to $rootScope and $compile
-        // so they are available to all tests in this describe block
-        beforeEach(inject(function (_$compile_, _$rootScope_) {
-            // The injector unwraps the underscores (_) from around the parameter names when matching
-            $compile = _$compile_;
-            $rootScope = _$rootScope_;
-        }));
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
 
-        it('Replaces the element with the appropriate content', function () {
+        $rootScope.layersSelectorData = {
+            option1: {name: "option 1"},
+            option2: {name: "option 2"}
+        };
+        $rootScope.item = {option1: {name: "option 1"}};
+        // Compile a the component: ui-leaflet-layers-selector
+        element = $compile('<ui-leaflet-layers-selector items="layersSelectorData" ng-model="item"></ui-leaflet-layers-selector>')($rootScope);
+        // fire all the watches
+        $rootScope.$digest();
+    }));
 
-            this.layersSelectorData = {
-                option1: {name: "option 1"},
-                option2: {name: "option 2"}
-            };
-            this.item = {option1: {name: "option 1"}};
-            // Compile a the component: ui-leaflet-layers-selector
-            var element = $compile('<ui-leaflet-layers-selector items="layersSelectorData" ng-model="item"></ui-leaflet-layers-selector>')($rootScope);
-            // fire all the watches
-            $rootScope.$digest();
-            // Check that the compiled element contains the templated content
-            expect(element.find('li').length).toBe(2);
-        });
+    it('Replaces the element with the appropriate content', function () {
+
+
+        // Check that the compiled element contains the templated content
+        expect(element.find('li').length).toBe(2);
     });
 });
+

7aa46cc28f3767896bdaed19e1ad7ad09c6a3236
racheliff <racheliff@gmail.com>
2016-02-21 15:49:50 +0200

    Update layerSelector.js

--- components/layerSelector/layerSelector.js
+++ components/layerSelector/layerSelector.js
@@ -3,16 +3,15 @@ var extensionApp = angular.module('ui-leaflet-extensions', ['ui-leaflet']);
 extensionApp.component('uiLeafletLayersSelector', {
     templateUrl: 'components/layerSelector/index.tmpl.html',
     bindings: {
-        items: '=',
-        currentItem: '='
+        tilesList: '=',
     },
     require: {
         leaflet: '^leaflet'
     },
     controller: function layerSelectorCtrl($scope) {
         this.show = true;
-        this.selectVal = function (index) {
-            this.currentItem = Object.keys(this.items)[index];
+        this.selectVal = function (item) {
+            this.leaflet.getLeafletScope().tiles = item;
         };
         this.$onInit = function() {
             console.log(this.leaflet);
